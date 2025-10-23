33758f8e03ddd9148ef3139e6533b439ba2ccad5
vanda <vanda@vandas-MacBook-Pro.local>
2025-10-23 10:46:03 +0700

    finishing app routing

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,7 +1,29 @@
+import 'package:get/get.dart';
+import 'package:tribun_app/bindings/home_bindings.dart';
+import 'package:tribun_app/screens/home_screen.dart';
+import 'package:tribun_app/screens/news_detail_screen.dart';
+import 'package:tribun_app/screens/splash_screen.dart';
+
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
   static const INITIAL = Routes.SPLASH;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashScreen(),
+    ),
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeScreen(),
+      binding: HomeBindings()
+    ),
+    GetPage(
+      name: _Paths.NEWS_DETAIL,
+      page: () => NewsDetailScreen()
+    )
+  ];
 }
